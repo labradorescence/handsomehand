9a463bdfa51802dedd5cd1fc3b613c3a20543861
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-02-26 15:20:34 -0500

    orange background / form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,23 +9,23 @@ function ContactForm() {
     Intake Fields:
     
     <form>
-        <input type="text"></input>
-    </form>
-    
-    1. Name of contact who will be in communication with Handsome Hand Signs for the duration of the project
-    2. Contact's phone number
-    3. Contact's email address
-    4. Contact's shipping address
-    5. Physical location of project
-    6. Are you interested in a custom design, or do you already have a design to work from?
-    7. Briefly describe the project and your brand, if applicable
-    8. Please upload 5-7 reference images, including your logo ((is it possible to do an upload form here?))
-    9. If the design will be painted directly onto a surface, such as a wall or vehicle, please provide dimensions and a description of the surface. Please include maximum working height from ground, and any other details related to access and conditions of the working area.
-    10. If you are ordering a manufactured sign (wood, metal, canvas etc), please provide dimensions, whether the sign will be single or double-sided, and if you would like hanging hardware to be included. 
-    11. Please provide the exact copy (text) for the project, as well as a description of any graphics. Reference photos can be uploaded in line 8.
-    12. Please provide any deadlines or relevant timelines for this project
-    13. Please provide any budget boundaries 
-    14. Please provide any additional information, comments or questions here
+        1. Your name: <input type="text"></input><br></br>
+        2. Your phone: <input type="text"></input><br></br>
+        3. Your email: <input type="text"></input><br></br>
+        4. Shipping address: <input type="text"></input><br></br>
+        5. Physical location of project : <input type="text"></input><br></br>
+        <input type="text"></input><br></br>
+        6. Are you interested in a custom design, or do you already have a design to work from?<input type="text"></input><br></br>
+        7. Briefly describe the project and your brand, if applicable<input type="text"></input><br></br>
+        8. Please upload 5-7 reference images, including your logo ((is it possible to do an upload form here?))<input type="text"></input><br></br>
+        9. If the design will be painted directly onto a surface, such as a wall or vehicle, please provide dimensions and a description of the surface. Please include maximum working height from ground, and any other details related to access and conditions of the working area.<input type="text"></input><br></br>
+        10. If you are ordering a manufactured sign (wood, metal, canvas etc), please provide dimensions, whether the sign will be single or double-sided, and if you would like hanging hardware to be included. <input type="text"></input><br></br>
+        11. Please provide the exact copy (text) for the project, as well as a description of any graphics. Reference photos can be uploaded in line 8.<input type="text"></input><br></br>
+        12. Please provide any deadlines or relevant timelines for this project<input type="text"></input><br></br>
+        13. Please provide any budget boundaries <input type="text"></input><br></br>
+        14. Please provide any additional information, comments or questions here
+        <input type="text"></input><br></br>
+        </form>
     </p>
     </div>
     )
