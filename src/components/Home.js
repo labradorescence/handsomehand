1407813addb3156f9247dd83ad763e048970e89c
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-02-28 12:44:39 -0500

    home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../App.css';
+import './Home.css';
 
 function Home() {  
+
     return (
     <div>       
-    <h1> HOME </h1>
+      <h1 className = 'primary'> HOME </h1>
     </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../App.css';
 import { Link } from 'react-router-dom';
+import './Nav.css'
 
 function Nav() {  
     return (
