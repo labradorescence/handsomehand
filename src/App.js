2ccea5a38559f7e7f68c88a592bdb023f79f9bca
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-03-01 11:16:20 -0500

    styled-components installed

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ import Nav from './components/Nav';
 import About from './components/About';
 import Contact from './components/Contact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components'
 
+const Button = styled.button`
+  font-family: sans-serif;
+  color: white;
+  background-color: orange;
+`
 
 function App() {  
+
     return (
       <Router>
           <div className="App">
             <Nav />
+            <Button> styled button </Button>
+
             <Switch>
             
             <Route path="/" exact component={Home}/>
