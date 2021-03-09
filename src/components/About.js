fc767331545eda4111c36a87476531632d780d0c
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-03-09 17:29:49 -0500

    testimonial

--- src/components/About.js
+++ src/components/About.js
@@ -21,6 +21,13 @@ function About() {
     Heidi T is an artist, traveler, and owner/operator of Handsome Hand Signs. Influenced by punk rock, graffiti and unconventional approaches, her creations possess a charming and unruly spirit that catch the eye, but still play by the rules. Heidi painted her first sign in 2009 as barter for a hostel in Central America, and officially joined the world of small business in 2018 with a brick and mortar in a rural mountain town. Her business model remains fluid as she prefers to travel for projects whenever possible.
     </p>
     
+    <br></br>
+    <br></br>
+
+    <h2> testimonials </h2>
+    <p>"Heidi has been sólo’s sign painter since 2013. We started off with a small menu sign, then an entrance sign, and more. Over the years sólo grew as a cafe and Heidi grew as a sign painter, our signs and our relationship did as well. Sólo wouldn’t be the same without the creative touch of Heidi’s intentional and creative signs sprinkled about the cafe. I have always trusted her vision and given her complete creative control, especially after years of getting to know each other. I am always blown away whether it's a new version of the same sign or a completely new concept shows up, revealing her development and growth as an artist over time." - Lauren, owner of sólo espresso </p>
+
+    <p> "Heidi from Handsome Hand Signs painted a sign for my soap company. She is such a talented artist! She helped me pick the design and colors and I couldn't have been happier. The classic artistry lends a true authentic touch to my sign that I feel proud to use to represent my business. Excellent service all around. You'd be lucky to have a sign by her!" - Sarah, owner of Goldie's Natural Beauty </p>
     </div>
     )
 }
