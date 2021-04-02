19c0af9960e1d24e5db62afe0278c56c76682e03
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-04-02 11:34:51 -0400

    form react count

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,6 +8,8 @@ export default function Form(){
         passworld: ""
     })
 
+    const [count, setCount] = useState(1)
+
     const updateForm = (e) => {
         setForm ({
             ...form,
@@ -18,7 +20,11 @@ export default function Form(){
 
     return (
         <div>
+
+            <h1> {count} of 3 </h1>
             <form onSubmit = { () => alert(`submitted email: ${form.email} name: ${form.name} passworld: ${form.password}`)}>
+
+                {count === 1  ?  (  //when the page count is 1 show this 
                 <div className = "form">
                 <label> Email </label>
                 <input 
@@ -29,7 +35,10 @@ export default function Form(){
                 value = {form.email}
                 />
                 </div>
+                ):null} {/* if the page count is not equal to 1 then null */}
 
+
+                {count === 2  ?  (
                 <div className = "form">
                 <label> Name </label>
                 <input 
@@ -40,7 +49,9 @@ export default function Form(){
                 value = {form.name}
                 />
                 </div>
+                ):null} 
 
+                {count === 3  ?  (
                 <div className = "form">
                 <label> Password </label>
                 <input 
@@ -50,11 +61,22 @@ export default function Form(){
                 onChange ={updateForm} 
                 value = {form.password}
                 />
-                </div>
                 <button type = "submit"> submit</button>
+                </div>
+                ):null} 
+
+
             </form>
-            <button type = "submit"> prev </button>
-            <button type = "submit"> next </button>
+            <button 
+            type = "submit" 
+            onClick = {() => setCount(count -1)} //decrease the page count
+            disabled = {count < 2} //don't do anytying when the page is the first page
+            > prev </button>
+            <button 
+            type = "submit" 
+            onClick = {() => setCount(count +1)}
+            disabled = {count > 2}
+            > next </button>
         </div>
     )
 }
