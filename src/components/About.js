9407287c2497da6e7a3304bd9e7eccfa1ac374a8
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-04-14 10:29:57 -0400

    css

--- src/components/About.js
+++ src/components/About.js
@@ -4,30 +4,30 @@ function About() {
     return (
     <div className="aboutPage">       
     
+    <div className = "about">
     <h1> ABOUT </h1>
-    <br></br>
-    <br></br>
+
     <p>
     Handsome Hand Signs is a small sign shop in Northern California specializing in lettering and graphics that are designed and painted completely by hand. Logos, Murals, Truck Lettering, Windows, Wooden Signs & Gold Leaf are among our offerings.
     </p>
-    <br></br>
-    <br></br>
+
     <p>
     Many people are surprised to hear that Sign Painting is (arguably) the oldest trade in the world. For centuries, Sign Painters have harnessed language and letterform in artful ways to shape culture through commerce, direction and expression. It was only as recent as the 1950's that computers, machines and vinyl adhesive started to take the place of hand lettering. When you choose a hand-painted sign, you are not only participating in a rich history of human creativity, but your sign is guaranteed to have soul!
     </p>
-    <br></br>
-    <br></br>
+
     <p>
     Heidi T is an artist, traveler, and owner/operator of Handsome Hand Signs. Influenced by punk rock, graffiti and unconventional approaches, her creations possess a charming and unruly spirit that catch the eye, but still play by the rules. Heidi painted her first sign in 2009 as barter for a hostel in Central America, and officially joined the world of small business in 2018 with a brick and mortar in a rural mountain town. Her business model remains fluid as she prefers to travel for projects whenever possible.
     </p>
-    
-    <br></br>
-    <br></br>
+    </div>
+
+
+    <div className = "testimonial">
 
     <h2> testimonials </h2>
     <p>"Heidi has been sólo’s sign painter since 2013. We started off with a small menu sign, then an entrance sign, and more. Over the years sólo grew as a cafe and Heidi grew as a sign painter, our signs and our relationship did as well. Sólo wouldn’t be the same without the creative touch of Heidi’s intentional and creative signs sprinkled about the cafe. I have always trusted her vision and given her complete creative control, especially after years of getting to know each other. I am always blown away whether it's a new version of the same sign or a completely new concept shows up, revealing her development and growth as an artist over time." - Lauren, owner of sólo espresso </p>
 
     <p> "Heidi from Handsome Hand Signs painted a sign for my soap company. She is such a talented artist! She helped me pick the design and colors and I couldn't have been happier. The classic artistry lends a true authentic touch to my sign that I feel proud to use to represent my business. Excellent service all around. You'd be lucky to have a sign by her!" - Sarah, owner of Goldie's Natural Beauty </p>
+    </div>
     </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,60 +1,47 @@
-import React from 'react'
+import React from 'react';
 import Form from './Form';
+import './Contact.css';
 
 function Contact() {  
     return (
     <div className="contactPage">       
     <h1> WORK WITH ME </h1>
-    <br></br>
-    <br></br>
+
     <Form></Form>
-    <br></br>
-    <br></br>
-    <div>
+
+    <div className="contactInfo">
     <p>
     Handmade lettering is a priceless investment for any business, brand or personal project that needs to stand apart. When you work with me, you are getting the benefit of 12 years of letterform obsession, a keen eye for color and spatial design, as well as thoughtful and generous consulting around small design decisions that make all the difference.
     </p>
-    <br></br>
-    <br></br>
+
     <p>
     If you are interested in working with me, kindly review the following information and fill out the intake form below. I will receive an email with the details of your project, and follow up as soon as I am able to. If you do not feel like your project falls within the relevance of this form, please email me to set up a phone call.
     </p>
-    <br></br>
-    <br></br>
+
     <p>
     handsomehandsigns@gmail.com
     </p>
-    <br></br>
-    <br></br>
+
     <p>
     Thank you for choosing hand painted signs! I look forward to hearing from you.
     </p>
-    <br></br>
-    <br></br>
+
     <p>
-    Terms:
-    <br></br>
-    <br></br>
-    ~Once you receive a quote, I ask for 50% to begin work, and 50% upon completion of the project or receipt of the sign. 
-    <br></br>
-    <br></br>
-    ~For fabricated wooden signs, you may be charged CA sales tax at a rate of .0725%
-    <br></br>
-    <br></br>
-    ~I offer shipping via USPS for signs under 65 lbs and 115 total inches
-    <br></br>
-    <br></br>
-    ~I offer regional delivery
-    <br></br>
-    <br></br>
-    ~I do not offer commercial installation
-    <br></br>
-    <br></br>
+    Terms:<br></br>
+
+    ~Once you receive a quote, I ask for 50% to begin work, and 50% upon completion of the project or receipt of the sign. <br></br>
+
+    ~For fabricated wooden signs, you may be charged CA sales tax at a rate of .0725% <br></br>
+
+    ~I offer shipping via USPS for signs under 65 lbs and 115 total inches <br></br>
+
+    ~I offer regional delivery <br></br>
+
+    ~I do not offer commercial installation <br></br>
+
     ~Any of my designs are not to be duplicated or reproduced, unless we discuss an "unlimited use" package. Reproduction without my permission may result in legal repercussions. 
     </p>
     </div>
-
-    {/* <ContactForm></ContactForm> */}
 
     </div>
     )
