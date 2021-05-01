47b0445bb3ec7a533a870797841326637e992740
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-05-01 15:34:30 -0500

    about nav glitch

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,9 +13,9 @@ function Nav() {
 
                 <div clasName = "navAbout">
 
+                <Link to="/about" className = "navAboutImg"></Link>
 
-
-                    <Link to="/about" className = "navAboutImg"></Link>
+                {/* <Link to="/about" className = "navAbout"> ABOUT </Link> */}
 {/* 
                 logo: https://i.imgur.com/fJJSiWb.png
                 about: https://i.imgur.com/asK3XbA.png
