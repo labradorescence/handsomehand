0c96ffce6dd868ffecdcc317afad231cac472519
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-05-01 11:18:25 -0500

    nav responsive

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -19,9 +19,6 @@ export default function Portfolio (){
 
             <img className = "single-img" src={"https://i.ibb.co/SJXgVBH/image00002.jpg"} alt="image00002" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/sFWkwKx/image00003.jpg"} alt="image00003" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/M9KzQSY/image00004.jpg"} alt="image00004" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/CJZCc46/image00005.jpg"} alt="image00005" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/W0WHzhZ/image00006.jpg"} alt="image00006" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/Vqfv9ns/image00007.jpg"} alt="image00007" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/jDRTx27/image00008.jpg"} alt="image00008" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/M8Cj3qD/image00009.jpg"} alt="image00009" border="0"/>
@@ -36,11 +33,12 @@ export default function Portfolio (){
             <img className = "single-img" src={"https://i.ibb.co/S73ZmHv/image00019.jpg"} alt="image00019" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/zQmK3dF/image00020.jpg"} alt="image00020" border="0"/>
             <img className = "single-img" src={"https://i.ibb.co/GppFJsL/image00021.jpg"} alt="image00021" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/HGNy4NP/image00022.jpg"} alt="image00022" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/9HyJw3k/image00023.jpg"} alt="image00023" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/cybYT6Y/image00017.jpg"} alt="image00017" border="0"/>
-            <img className = "single-img" src={"https://i.ibb.co/HtQjbYK/image00025.jpg"} alt="image00025" border="0"/>
+            <img className = "single-img" src={"https://i.ibb.co/HtQjbYK/image00025.jpg"} alt="ice" border="0"/>
+            <img className = "single-img" src={"https://i.ibb.co/cybYT6Y/image00017.jpg"} alt="iloveyoucar" border="0"/>           
+            <img className = "single-img" src={"https://i.ibb.co/CJZCc46/image00005.jpg"} alt="image00005" border="0"/>
 
+            <img className = "single-img" src={"https://i.ibb.co/W0WHzhZ/image00006.jpg"} alt="image00006" border="0"/>
+            <img className = "single-img" src={"https://i.ibb.co/HGNy4NP/image00022.jpg"} alt="image00022" border="0"/>
             </div>
 
         </div> 
